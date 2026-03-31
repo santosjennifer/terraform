d7059013c85cea714f0b645fe5d90bd391f2f341
santosjennifer <asantos.jennifer@gmail.com>
2026-03-31 19:27:20 -0300

    formate file

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "tfstate" {
-  bucket = "tfstate-terraform-santos"
+  bucket        = "tfstate-terraform-santos"
   force_destroy = true
 
-   tags = {
-      Environment = "dev"
-      ManageBy = "Terraform"
-   }
+  tags = {
+    Environment = "dev"
+    ManageBy    = "Terraform"
+  }
 }
